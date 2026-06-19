89c81c253a5f58317f8e04a77d2f8ed838e4dfbd
GalibDev <mirza.galib.palash>
2026-06-19 16:20:47 +0600

    feat: add user report history API

--- src/routes/report.routes.js
+++ src/routes/report.routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { createReport } from '../controllers/report.controller.js';
+import { createReport, getMyReports } from '../controllers/report.controller.js';
 import { verifyToken } from '../middleware/auth.middleware.js';
 import { asyncHandler } from '../utils/async-handler.js';
 
 const router = Router();
 
-router.post('/', verifyToken, asyncHandler(createReport));
+router.use(verifyToken);
+router.get('/mine', asyncHandler(getMyReports));
+router.post('/', asyncHandler(createReport));
 
 export default router;
